cb6ad49d760670862c955d3ef3ec6ae15614715a
kaspi174 <kczajka174@gmail.com>
2023-04-07 21:59:00 +0100

    Board printing and player choice

--- tictactoe/src/App.java
+++ tictactoe/src/App.java
@@ -1,5 +1,112 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
+
+    static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+    static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+        {'-', '+', '-', '+', '-'},
+        {' ', '|', ' ', '|', ' '},
+        {'-', '+', '-', '+', '-'},
+        {' ', '|', ' ', '|', ' '}};
+
+
+       
+
+       Scanner scan = new Scanner(System.in);
+
+       while(true){
+       System.out.println("Eneter your placement (1-9)");
+       int position = scan.nextInt();
+
+       System.out.println(position);
+
+       placePoint(gameBoard, position, "player");
+       Random rand = new Random();
+       int cpuPosition = rand.nextInt(9) + 1;
+       placePoint(gameBoard, position, "cpu");
+ 
+       printGameBoard(gameBoard);
+
+       }    
     }
 }
+
+public static void printGameBoard(char[][] gameBoard){
+    for(char[] row : gameBoard){
+        for(char c : row){
+            System.out.println(c);            }
+    }
+    System.out.println();
+}
+
+public static void placePoint(char[][] gameBoard, int position, String user){
+
+    char symbol = 'X';
+
+    if(user.equals("player")){
+        symbol = 'X';
+    }else if(user.equals("cpu")){
+        symbol ='O';
+    }
+
+
+    switch(position){
+        case 1:
+        gameBoard[0][0] = symbol;
+        break;
+        case 2:
+        gameBoard[0][2] = symbol;
+        break;
+        case 3:
+        gameBoard[0][4] = symbol;
+        break;
+        case 4:
+        gameBoard[2][0] = symbol;
+        break;
+        case 5:
+        gameBoard[2][2] = symbol;
+        break;
+        case 6:
+        gameBoard[2][4] = symbol;
+        break;
+        case 7:
+        gameBoard[4][0] = symbol;
+        break;
+        case 8:
+        gameBoard[4][2] = symbol;
+        break;
+        case 9:
+        gameBoard[4][4] = symbol;
+        break;
+    }
+}
+
+public static String checkWinner() {
+
+    List topRow = Arrays.asList(1, 2, 3);
+    List midRow = Arrays.asList(4, 5, 6);
+    List botRow = Arrays.asList(7, 8, 9);
+
+    List leftCol = Arrays.asList(1, 4, 7);
+    List midCol = Arrays.asList(2, 5, 8);
+    List rightCol = Arrays.asList(3, 6, 9);
+
+    List cross1 = Arrays.asList(1, 5, 9);
+    List cross2 = Arrays.asList(7, 5, 3);
+
+
+
+
+    return "";
+    
+}
+}
+
